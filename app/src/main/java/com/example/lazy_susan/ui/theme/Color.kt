d53d9a6f0e9fcc904b395a071af8f922ba303571
phungus <67817630+Justin-Phung@users.noreply.github.com>
2025-03-05 14:13:52 -0800

    test gray

--- app/src/main/java/com/example/lazy_susan/ui/theme/Color.kt
+++ app/src/main/java/com/example/lazy_susan/ui/theme/Color.kt
@@ -2,14 +2,8 @@ package com.example.lazy_susan.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
-val Red40 = Color(0xFFF96167)
-val Yellow = Color(0xFFF9E795)
+val PicnicRed = Color(0xFFF96167)
+val HoneyMustardYellow = Color(0xFFF9E795)
 val MediumGray = Color(0xFF656565)
+val LightGray = Color(0xFFDDDDDD)
+val DarkGray = Color(0xFF7D7D7D)
