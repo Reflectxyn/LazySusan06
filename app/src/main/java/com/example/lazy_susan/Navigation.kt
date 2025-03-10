f6d945024b4303d4feaec25126c8d2ac584cff09
phungus <67817630+Justin-Phung@users.noreply.github.com>
2025-03-10 13:52:57 -0700

    making up for lost progress

--- app/src/main/java/com/example/lazy_susan/Navigation.kt
+++ app/src/main/java/com/example/lazy_susan/Navigation.kt
@@ -1,23 +1,17 @@
 package com.example.lazy_susan
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -37,25 +31,15 @@ fun LazySusanAppBar(
     canNavigateBack: Boolean,
     navigateUp: () -> Unit = {}
 ) {
-    TopAppBar(
-        title = { Text(
-            text = stringResource(currentScreen.title),
-            modifier = Modifier.fillMaxWidth()
-        ) },
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(currentScreen.title),
+                style = MaterialTheme.typography.headlineLarge
+            ) },
         colors = TopAppBarDefaults.mediumTopAppBarColors(
             containerColor = HoneyMustardYellow
-        ),
-        modifier = Modifier.height(116.dp),
-        navigationIcon = {
-            if (canNavigateBack) {
-                IconButton(onClick = navigateUp) {
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                        contentDescription = stringResource(R.string.back_button)
-                    )
-                }
-            }
-        }
+        )
     )
 }
 
--- app/src/main/java/com/example/lazy_susan/ui/theme/Color.kt
+++ app/src/main/java/com/example/lazy_susan/ui/theme/Color.kt
@@ -2,4 +2,8 @@ package com.example.lazy_susan.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val HoneyMustardYellow = Color(0xFFD0BCFF)
+val HoneyMustardYellow = Color(0xFFF9E795)
+val PicnicTableRed = Color(0xFFF96167)
+val LightGray = Color(0xFFDDDDDD)
+val MediumGray = Color(0xFFD9D9D9)
+val DarkGray = Color(0xFF7D7D7D)
